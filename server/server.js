fb093e5d774f67c5a9a613353d66ae00f20caf9a
Keerthi <tnvk.keerthi@gmail.com>
2018-11-13 06:36:20 +0000

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,7 +13,20 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket){
     console.log("new user connected");
     
+   // alerting others when new user joins 
+    socket.broadcast.emit('newMessage', {
+            from : 'admin',
+            text : 'new user joined',
+            createdAt : new Date().getTime() 
+        });
   
+  // welcome msg to new user
+    socket.emit('newMessage', {
+            from : 'admin',
+            text : 'welcome user',
+            createdAt : new Date().getTime() 
+   });
+   
     socket.on('createMessage', function(message){
       console.log('create message', message);
       io.emit('newMessage', {
@@ -21,6 +34,13 @@ io.on('connection', function(socket){
           text : message.text,
           createdAt : new Date().getTime()
       });
+      
+      
+        // socket.broadcast.emit('newMessage', {
+        //     from : message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime() 
+        // });
     });
     
     socket.on('disconnect', () => {
